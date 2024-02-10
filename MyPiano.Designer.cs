39f8a7587000dbae40fcd39d79921bc86f22c624
JeffDChapman <jeffdchapman@hotmail.com>
2024-02-10 10:12:05 -0800

    Initial Release
    First release of a program to play random notes in phrases

---
+++ MyPiano.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace PlayNotes
+{
+    partial class MyPiano
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGo = new System.Windows.Forms.Button();
+            this.cbContinuous = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(90, 18);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(100, 52);
+            this.btnGo.TabIndex = 0;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // cbContinuous
+            // 
+            this.cbContinuous.AutoSize = true;
+            this.cbContinuous.Location = new System.Drawing.Point(82, 94);
+            this.cbContinuous.Name = "cbContinuous";
+            this.cbContinuous.Size = new System.Drawing.Size(116, 24);
+            this.cbContinuous.TabIndex = 1;
+            this.cbContinuous.Text = "Continuous";
+            this.cbContinuous.UseVisualStyleBackColor = true;
+            // 
+            // MyPiano
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 135);
+            this.Controls.Add(this.cbContinuous);
+            this.Controls.Add(this.btnGo);
+            this.Name = "MyPiano";
+            this.Text = "My Piano";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.CheckBox cbContinuous;
+    }
+}
+
---
+++ MyPiano.cs
@@ -0,0 +1,156 @@
+﻿using NAudio.Wave.SampleProviders;
+using NAudio.Wave;
+using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+namespace PlayNotes
+{
+    public partial class MyPiano : Form
+    {
+        private bool stopPressed;
+        private int minFrequency = 300;
+        private int maxFrequency = 1200;
+        private int minNoteDuration = 500;
+        private int numOfPhrases = 6;
+        private int freqNormalizer = 16;
+        private float SweepSize = 1.03f;
+
+        private struct NoteStruct
+            { public int NoteTime; public bool NoteRest; public double Notefrequency; }
+        private List<NoteStruct> onePhrase;
+
+        public MyPiano()
+        {
+            InitializeComponent();
+            if (!this.Visible) { btnGo_Click(this, null);  }
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            if (btnGo.Text == "Go") { btnGo.Text = "Stop"; stopPressed = false; }
+            else if (!cbContinuous.Checked)
+                { btnGo.Text = "Stop"; stopPressed = true; btnGo.Text = "Go"; return; }
+
+            // Create a new instance of WaveOutEvent
+            using (var waveOut = new WaveOutEvent())
+            {
+                // Create a new instance of SignalGenerator
+                var signalGenerator = new SignalGenerator((int)(maxFrequency * 1.5), 1);
+                signalGenerator.Type = SignalGeneratorType.Sweep;
+                signalGenerator.SweepLengthSecs = 2;
+
+                // Create a new WaveProvider32 with the SignalGenerator
+                var waveProvider = signalGenerator.ToWaveProvider();
+
+                // Initiate playback
+                waveOut.Init(waveProvider);
+
+                // Random number generator
+                Random random = new Random();
+
+                // Generate and play random phrases
+                for (int i = 0; i < numOfPhrases; i++)
+                {
+                    onePhrase = new List<NoteStruct>();
+                    BuildNotePhrase(onePhrase, random);
+
+                    for (int k = 0; k < 2; k++)
+                    {
+                        for (int j = 0; j < onePhrase.Count; j++)
+                        { 
+                            unpackAndPlay(onePhrase[j], signalGenerator, waveOut);
+                            if (stopPressed) { waveOut.Stop(); return; }
+                        }
+                        Console.WriteLine("----------------");
+                        System.Threading.Thread.SpinWait(2000);
+                        System.Threading.Thread.Sleep(1000);
+                        waveOut.Stop();
+                        System.Threading.Thread.SpinWait(2000);
+                        System.Threading.Thread.Sleep(1000);
+                    }
+                }
+
+                if (!cbContinuous.Checked)
+                {
+                    // Cleanup
+                    waveOut.Stop();
+                    this.Close();
+                    Application.Exit();
+                }
+                btnGo_Click(this, null);
+            }
+        }
+
+        private void unpackAndPlay(NoteStruct noteStruct, SignalGenerator signalGenerator, WaveOutEvent waveOut)
+        {
+            int playTime;
+            bool playRest;
+            double frequency;
+            //GetAnote(random, out playTime, out playRest, out frequency);
+            playTime = noteStruct.NoteTime;
+            playRest = noteStruct.NoteRest;
+            frequency = noteStruct.Notefrequency;
+
+            signalGenerator.Frequency = (double)frequency;
+            signalGenerator.FrequencyEnd = frequency * SweepSize;
+            Console.Write(signalGenerator.Frequency.ToString() + " ");
+
+            if (playRest)
+            {
+                waveOut.Stop();
+                Console.WriteLine("X");
+                Console.Write("Rest ");
+            }
+            else { waveOut.Play(); }
+
+            //waveOut.Play();
+
+            // Wait for the duration of the note
+
+            Console.WriteLine(playTime.ToString());
+
+            System.Threading.Thread.SpinWait(2000);
+            System.Threading.Thread.Sleep(playTime);
+            Application.DoEvents();
+        }
+
+        private void BuildNotePhrase(List<NoteStruct> onePhrase, Random random)
+        {
+            int phraseLen = random.Next(3, 9);
+            for (int i = 0; i < phraseLen; i++)
+            {
+                int playTime;
+                bool playRest;
+                double frequency;
+                GetAnote(random, out playTime, out playRest, out frequency);
+                NoteStruct myNote = new NoteStruct();
+                myNote.NoteTime = playTime;
+                myNote.NoteRest = playRest;
+                myNote.Notefrequency = frequency;
+                onePhrase.Add(myNote);
+            }
+
+        }
+
+        private void GetAnote(Random random, out int playTime, out bool playRest, out double frequency)
+        {
+            // Generate a normalized random frequency within the specified range
+            frequency = (double)random.Next(minFrequency, maxFrequency + 1);
+            frequency = NormalizeFreq(frequency);
+
+            playTime = minNoteDuration * random.Next(1, 3);
+            playRest = false;
+            if (random.Next(1, 100) > 85) { playRest = true; }
+        }
+
+        private float NormalizeFreq(double frequency)
+        {
+            double logFreq = Math.Log10(frequency);
+            int logFreq10 = Convert.ToInt16(logFreq * freqNormalizer);
+            return (float) (Math.Pow(10, ((double)logFreq10 / freqNormalizer)));
+        }
+    }
+}
+
